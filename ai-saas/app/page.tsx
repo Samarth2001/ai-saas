1fd02e24cafe2e01d662b4bde5a43de7f250219f
Samarth2001 <samarthsr2001@gmail.com>
2023-12-30 02:26:24 +0530

    update

--- ai-saas/app/page.tsx
+++ ai-saas/app/page.tsx
@@ -1,7 +1,8 @@
+import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-   <p>deploying soon!! resolving issues</p>
-  )
+    <Button></Button>
+    )
 }
